5290ae90039c156a967d6ba4e1635af30e5be04f
kristjanjansen <kristjan.jansen@gmail.com>
2018-05-18 06:13:35 +0300

    flower

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ import Box from "./components/Box.js";
 import Spoke from "./components/Spoke.js";
 import Spiral from "./components/Spiral.js";
 import Shutter from "./components/Shutter.js";
+import Flower from "./components/Flower.js";
 
 new Vue({
   el: "#app",
-  components: { Scene, Spinner, Box, Spoke, Spiral, Shutter },
-  data: () => ({ size: 200, speed: 1800, speed2: 120, count: 108, radius: 5 }),
+    components: { Scene, Spinner, Box, Spoke, Spiral, Shutter, Flower },
+  data: () => ({
+    size: 200,
+    speed: 1800,
+    speed2: 120,
+    count: 108,
+    radius: 5,
+    divide: 104
+  }),
   template: `
         <div>
             <h5>Size: {{size}} px</h5>
@@ -21,37 +29,32 @@ new Vue({
             <button @click="speed = 1800; count = 108">33⅓ RPM</button>
             <button @click="speed = 1333; count = 80">45 RPM</button>
 
-
-            <!--h5>Speed2: {{speed2}} ms {{speed2/1000}} s</h5>
-            <input type="range" v-model="speed2" min="0.001" max="1000" step="0.001" />
-            -->
-
             <h5>Spoke count: {{count}}, degrees: {{360 / count}} </h5>
             <input v-model="count" max="360" />
             <input type="range" v-model="count" max="360" />
             
             <h5>Radius: {{radius}}</h5>
             <input type="range" v-model="radius" max="720" />
+
+            <h5>Flower divide: {{divide}}</h5>
+            <input type="range" v-model="divide" min="1" :max="count" />
             
             <Scene :size="size">
                 <Spinner :speed="speed" direction="">
                     <Box :size="size / 2" fill="none" />
                 </Spinner>
-                <Shutter :size="size" :speed2="speed2" />
             </Scene>
-
-            <!--Scene :size="size">
-                <Spinner :speed="speed">
-                    <Spiral :size="size / 2" :count="1000" :radius="radius"/>
-                </Spinner>
-                <Shutter :size="size" :speed2="speed2" />
-            </Scene-->
 
             <Scene :size="size">
                 <Spinner :speed="speed">
                     <Spoke :size="size / 2" :count="count" />
                 </Spinner>
-                <Shutter :size="size" :speed2="speed2" />
+            </Scene>
+
+            <Scene :size="size">
+                <Spinner :speed="speed">
+                    <Flower :size="size / 2" :count="count" :divide="divide"/>
+                </Spinner>
             </Scene>
 
         </div>
