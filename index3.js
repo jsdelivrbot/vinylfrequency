f022c9c1f5767ce242c577308aae3313633f1849
kristjanjansen <kristjan.jansen@gmail.com>
2018-08-19 16:42:57 +0300

    animation loops

--- index3.js
+++ index3.js
@@ -2,9 +2,54 @@ import Scene from "./components/Scene.js";
 import Spoke2 from "./components/Spoke2.js";
 import Spinner from "./components/Spinner.js";
 
+const LoopSquare = {
+  props: ['count', 'n', 'size'],
+  methods: {
+    t(x, y, r = 0) {
+      return `translate(${x},${y})`;
+    },
+    r(angle) {
+      return `rotate(${angle})`;
+    }
+  },
+  template: `
+    <g :transform="r(360 / count * n)" opacity="0.5">
+      <rect :x="size / -2" :y="size / -2" :width="size" :height="size" fill="var(--color-purple)" />
+      <rect :x="size / -2" :y="size / -2" :width="size / 2" :height="size / 2" fill="var(--color-yellow)" />
+    </g>
+  `
+}
+
+const LoopHorse = {
+  props: ["count", "n", "size"],
+  template: `
+      <g opacity="0.7">
+        <image :width="size" :href="'./images/horse/horse' + String(n % 15 + 1).padStart(3,'0') + '.png'" />
+     </g>
+  `
+};
+
+const LoopHammer = {
+  props: ['count', 'n', 'size'],
+  template: `
+      <g opacity="0.7">
+        <image :width="size" :href="'./images/hammer00' + [n % 20] + '.png'" />
+     </g>
+  `
+}
+
+const LoopMetropolis = {
+  props: ["count", "n", "size"],
+  template: `
+      <g opacity="1">
+        <image :width="size" :href="'./images/metropolis/metropolis' + String(n * 2 + 1).padStart(3,'0') + '.png'" />
+     </g>
+  `
+};
+
 new Vue({
   el: "#app",
-  components: { Scene, Spoke2, Spinner },
+  components: { Scene, Spoke2, Spinner, LoopSquare, LoopHammer, LoopMetropolis, LoopHorse },
   methods: {
     t(x, y, r = 0) {
       return `translate(${x},${y})`;
@@ -36,10 +81,7 @@ new Vue({
           <Spinner :speed="speed">
           <g v-for="(_,n) in Array.from({length: 100}).slice(0,count)" :transform="r(360 / count * n)"> 
           <g :transform="t(0,(sceneSize / 2 - size) * -1 * 0.9)"> 
-            <g :transform="r(360 / count * n)" opacity="0.5">
-              <rect :x="size / -2" :y="size / -2" :width="size" :height="size" fill="var(--color-purple)" />
-              <rect :x="size / -2" :y="size / -2" :width="size / 2" :height="size / 2" fill="var(--color-yellow)" />
-            </g>
+            <LoopHorse :count="count" :n="n" :size="size" />
           </g>        
           </g>
         </Spinner>
